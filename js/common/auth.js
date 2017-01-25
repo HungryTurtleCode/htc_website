46520588fabec4bba9b53684aabe87e9f9e2afb0
Adrian <adimandevelopment@gmail.com>
2017-01-25 05:15:12 +0000

    flesh out some todos

--- js/common/auth.js
+++ js/common/auth.js
@@ -14,6 +14,7 @@ class Auth{
   }
   facebookSignIn(){
     let provider = new firebase.auth.FacebookAuthProvider();
+    // TODO look into converting anon accounts into permanent as per https://firebase.google.com/docs/auth/web/anonymous-auth  Wed 25 Jan 2017 05:21:26 UTC
     return this.signInWithRedirect(provider);
   }
   googleSignIn(){
@@ -32,9 +33,24 @@ class Auth{
         return result;
       })
   }
+  signInWithUserPass(user, pass, pass1){
+    if(pass !== pass1){console.error('Passwords don\'t match')}
+
+    // TODO flesh out Wed 25 Jan 2017 05:32:38 UTC
+  }
+  forgottenPass(){
+    // TODO flesh out this method to call firebase's api for forgotten password Wed 25 Jan 2017 05:31:45 UTC
+  }
   onAuthChange(fn){
-    firebase.auth().onAuthStateChanged((user) => {
+    return firebase.auth().onAuthStateChanged((user) => {
       if(user){
+        if(user.isAnonymous){
+          // TODO store cookie with user uid Wed 25 Jan 2017 05:10:49 UTC
+        }else{
+          // TODO check if there is anonymous cookie data. If there is transfer user data to new account. Delete cookie Wed 25 Jan 2017 05:11:07 UTC
+          // TODO add user to active campaign and store a flag in firebase to show that email has been added to active campaign to avoid duplicate requests Wed 25 Jan 2017 05:30:20 UTC
+          // TODO sync user data to firebase Wed 25 Jan 2017 05:27:21 UTC
+        }
         if(fn){
           fn(user);
         }
