1b56f5d186e31ab1db63739702252a5fe186b672
Adrian <adimandevelopment@gmail.com>
2017-01-29 16:47:38 +0000

    add comment markup

--- js/posts/components/comment/comment.component.js
+++ js/posts/components/comment/comment.component.js
@@ -6,7 +6,31 @@ const CommentComponent = {
       data: '<'
   },
   template: `
-    <h3>This is a comment</h3>
+    <div class="comment-cont" ng-class="{'reply': $ctrl.data.isReply}">
+      <div class="header">
+        <div class="image-cont" style="background: url({{$ctrl.data.image}}); background-size: cover"></div>
+        <span class="name" ng-bind-html="$ctrl.formattedName()"></span>
+        <span class="date">{{::$ctrl.data.data * 1000 | date}}</span>
+
+      </div>
+      <div class="padding">
+        <div class="body">
+          <p>{{$ctrl.data.text}}</p>
+        </div>
+        <div class="footer">
+          <span class="score">
+            ({{$ctrl.data.score}})
+          </span>
+          <span class="up">
+            Up
+          </span>
+          <span class="down">
+            Down
+          </span>
+          <span class="reply">reply</span>
+        </div>
+      </div>
+    </div>
   `
 };
 
--- js/posts/components/comment/comment.controller.js
+++ js/posts/components/comment/comment.controller.js
@@ -1,7 +1,32 @@
-class ControllerName{
-  constructor() {
-    let a = 0;
+class commentController{
+  constructor($sce) {
+    this.$sce = $sce;
+  }
+
+  $onInit(){
+    this.data = {
+      isReply: false,
+      user_name: 'Adrian Henry',
+      image: 'https://scontent.flhr4-1.fna.fbcdn.net/v/t1.0-9/1484172_10151935197492795_852819600_n.jpg?oh=921dd9d6c50b90ccc87b5ab5f0d9a8c8&oe=5902A22E',
+      text: 'This is the comment that the user left on this post',
+      data: 1485704985,
+      score: 3
+    }
+  }
+  formattedName(){
+    let name = this.data.user_name.split(' ');
+    if(name[2]){
+      name[1] = '<span class="highlight-name">' + name[1] + '</span>';
+      name[2] = '<span class="accent-name">' + name[2] + '</span> <span class="wrote">wrote:</span>';
+    }else if(name[1]){
+      name[1] = '<span class="highlight-name">' + name[1] + '</span><span class="accent-name wrote"> wrote:</span>';
+    }else{
+      name[0] = name[0] + '<span class="highlight-name wrote"> wrote:</span>'
+    }
+    return this.$sce.trustAsHtml(name.join(' '));
   }
 }
 
-export default ControllerName;
+commentController.$inject = ['$sce'];
+
+export default commentController;
