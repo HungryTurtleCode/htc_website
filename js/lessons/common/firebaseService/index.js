019f34abacda2e9977fe609acb161d3311a5b2a3
Adrian <adimandevelopment@gmail.com>
2017-02-07 13:27:53 +0000

    removed the firebase service module from lessons

--- js/lessons/common/firebaseService/index.js
+++
@@ -1,9 +0,0 @@
-import angular from 'angular';
-import firebaseService from './firebase.service';
-
-const firebaseServiceComponent = angular
-  .module('firebaseService.module', [])
-  .service('firebaseService', firebaseService)
-  .name;
-
-export default firebaseServiceComponent;
