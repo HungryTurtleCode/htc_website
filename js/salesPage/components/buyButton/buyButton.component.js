f78f7ccc5811cfdce73ba43308fefc786e791760
Adrian <adimandevelopment@gmail.com>
2017-02-20 15:53:46 +0000

    display price for course that was retrieved from firebase. Ensuring it is up to date

--- js/salesPage/components/buyButton/buyButton.component.js
+++ js/salesPage/components/buyButton/buyButton.component.js
@@ -4,10 +4,18 @@ const BuyButton = {
   controller,
   template: `
     <div ng-hide="$ctrl.loading">
+      <div class="price">
+        <p class="amount">
+          {{$ctrl.courseData.price | currency:"$":0}}
+        </p>
+      </div>
       <button class="take-course"
         ng-click="$ctrl.takeCourse()">
         Take This Course
       </button>
+    </div>
+    <div class="buy-button-loading" ng-show="$ctrl.loading">
+      Loading...
     </div>
   `
 };
