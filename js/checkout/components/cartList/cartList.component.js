9907f9e713e5721e7744354b2465cd3bca563eb9
Adrian <adimandevelopment@gmail.com>
2017-03-02 18:11:40 +0000

    style the cart list

--- js/checkout/components/cartList/cartList.component.js
+++ js/checkout/components/cartList/cartList.component.js
@@ -7,9 +7,16 @@ const CartList = {
     <div ng-if="$ctrl.cart.length === 0">
       Loading...
     </div>
-    <div ng-repeat="item in $ctrl.cart">
-      <button ng-click="$ctrl.removeItem({item})">Remove</button>
-      {{item}}
+    <div ng-repeat="item in $ctrl.cart" class="cart-item">
+      <a ng-href="{{item.slug}}">
+        <img class="cart-img"
+          ng-src="{{item.img}}"/>
+      </a>
+      <a ng-href="{{item.slug}}">
+        {{item.title}}
+      </a>
+      <span class="price">{{item.price | currency:$:0}}</span>
+      <button ng-click="$ctrl.removeItem({item})">X</button>
     </div>
   `
 };
