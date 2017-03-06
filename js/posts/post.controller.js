3b4a2fd80b27a55dd9f3f63b0aa817ffb7a35dc2
Adrian <adimandevelopment@gmail.com>
2017-03-06 18:17:21 +0000

    extend the functionality of the getPageLocations method to handle removing query params

--- js/posts/post.controller.js
+++ js/posts/post.controller.js
@@ -26,6 +26,10 @@ class PostCommentController{
       if(arr[i] === '' || arr[i] === '#!'){
         arr.splice(i, 1);
       }
+      let matches = arr[i].match(/\?([^&]*)/);
+      if(matches){
+        arr[i] = arr[i].slice(0, matches.index);
+      }
     }
 
     let newArr = [arr[arr.length - 2], arr[arr.length - 1]];
