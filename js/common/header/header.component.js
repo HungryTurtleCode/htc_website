1ffd8e16d1e816de9ec4cf35a6e91cb666510cc7
Adrian <adimandevelopment@gmail.com>
2017-03-06 17:05:38 +0000

    display notifications in a popover

--- js/common/header/header.component.js
+++ js/common/header/header.component.js
@@ -6,49 +6,75 @@ const HeaderComponent = {
     <div class="header-panel">
       <div class="sign-in-register">
 
-        <button class="notification-btn"
-          ng-if="$ctrl.loggedIn && !$ctrl.loading">
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 88.516 88.516" style="enable-background:new 0 0 88.516 88.516;" xml:space="preserve" data-inboxsdk-session-id="1488814308998-0.6025413539877926">
-          <g>
+        <div class="relative">
+          <button class="notification-btn"
+            ng-click="$ctrl.notificationClick($event)"
+            ng-if="$ctrl.loggedIn && !$ctrl.loading">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 88.516 88.516" style="enable-background:new 0 0 88.516 88.516;" xml:space="preserve" data-inboxsdk-session-id="1488814308998-0.6025413539877926">
             <g>
-                <path d="M67.512,36.822C56.11,12.287,50.184,2.97,31.553,3.355c-6.635,0.137-5.041-4.805-10.1-2.93    c-5.055,1.876-0.717,4.62-5.889,8.863C1.051,21.2,2.389,32.221,9.119,58.487c2.838,11.062-6.836,11.605-3.008,22.33    c2.793,7.819,23.393,11.093,45.127,3.028c21.734-8.063,35.453-24.07,32.66-31.889C80.069,41.231,72.317,47.152,67.512,36.822z     M48.879,77.238c-19.41,7.202-35.363,2.965-36.037,1.083c-1.162-3.248,6.266-14.081,28.457-22.316    c22.193-8.234,34.576-5.181,35.871-1.553C77.936,56.594,68.291,70.036,48.879,77.238z M42.641,59.764    C32.493,63.53,25.44,67.837,20.877,71.715c3.211,2.918,9.23,3.63,15.23,1.404c7.637-2.833,12.326-9.337,10.471-14.526    c-0.021-0.063-0.055-0.119-0.078-0.181C45.248,58.826,43.963,59.274,42.641,59.764z"/>
-                  </g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <g>
-                  </g>
-                  <head/></svg>
-          <span ng-if="$ctrl.notifications.length">
-            {{$ctrl.notifications.length}}
-          </span>
-        </button>
+              <g>
+                  <path d="M67.512,36.822C56.11,12.287,50.184,2.97,31.553,3.355c-6.635,0.137-5.041-4.805-10.1-2.93    c-5.055,1.876-0.717,4.62-5.889,8.863C1.051,21.2,2.389,32.221,9.119,58.487c2.838,11.062-6.836,11.605-3.008,22.33    c2.793,7.819,23.393,11.093,45.127,3.028c21.734-8.063,35.453-24.07,32.66-31.889C80.069,41.231,72.317,47.152,67.512,36.822z     M48.879,77.238c-19.41,7.202-35.363,2.965-36.037,1.083c-1.162-3.248,6.266-14.081,28.457-22.316    c22.193-8.234,34.576-5.181,35.871-1.553C77.936,56.594,68.291,70.036,48.879,77.238z M42.641,59.764    C32.493,63.53,25.44,67.837,20.877,71.715c3.211,2.918,9.23,3.63,15.23,1.404c7.637-2.833,12.326-9.337,10.471-14.526    c-0.021-0.063-0.055-0.119-0.078-0.181C45.248,58.826,43.963,59.274,42.641,59.764z"/>
+                    </g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <g>
+                    </g>
+                    <head/></svg>
+            <span ng-if="$ctrl.notifications.length">
+              {{$ctrl.notifications.length}}
+            </span>
+          </button>
+          <div class="popover"
+            ng-click="$ctrl.stopPropagation($event)"
+            ng-if="$ctrl.notificationActive">
+              <div class="arrow"></div>
+
+              <div class="notification-item"
+                ng-click="$ctrl.clickNotificationItem(notif)"
+                ng-repeat="notif in $ctrl.notifications">
+                  <div class="top">
+                    <img ng-src="{{notif.image}}">
+                    <p class="reply-text">
+                      {{notif.user_name}} said something in a discussion you are involved in
+                    </p>
+                  </div>
+                  <div class="bottom">
+                    <p class="date">{{notif.date | date}}</p>
+                  </div>
+                  <!--<p>{{notif.text}}</p>
+                  <p>{{notif.firebase_id}}</p>
+                  <p>{{notif.notification_type}}</p>
+                  <p>{{notif.location}}</p>-->
+              </div>
+          </div>
+        </div>
 
         <a href="{{site.baseurl}}/account" ng-if="$ctrl.loggedIn" class="account">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve" data-inboxsdk-session-id="1488815250232-0.448238553788453">
--- js/common/header/header.controller.js
+++ js/common/header/header.controller.js
@@ -1,17 +1,32 @@
 class HeaderController{
-  constructor(auth, userData) {
+  constructor(auth, userData, $timeout) {
     this.auth = auth;
     this.userData = userData;
+    this.$timeout = $timeout;
   }
   $onInit(){
     this.loggedIn = false;
     this.loading = true;
     this.showSignIn = false;
     this.showLogOut = false;
+    this.notificationActive = false;
 
     this.auth.subscribeAuthChange(this.onAuthChange.bind(this));
     this.userData.getNotifications(
                   notifications => this.notifications = notifications);
+
+    window.addEventListener('click', event => {
+      this.$timeout(() => {
+        this.notificationClick(event);
+      });
+    });
+  }
+  notificationClick(e){
+    this.stopPropagation(e);
+    this.notificationActive = !this.notificationActive;
+  }
+  stopPropagation(e){
+    e.stopPropagation();
   }
   onAuthChange(user){
     this.loading = false;
@@ -27,8 +42,14 @@ class HeaderController{
   closeLogOut(){
     this.showLogOut = false;
   }
+  clickNotificationItem(item){
+    // TODO clear the notification Mon 06 Mar 2017 17:05:01 UTC
+
+    // TODO add query params to allow other functionality Mon 06 Mar 2017 17:05:26 UTC
+    window.location.href = item.location;
+  }
 }
 
-HeaderController.$inject = ['auth', 'userData'];
+HeaderController.$inject = ['auth', 'userData', '$timeout'];
 
 export default HeaderController;
