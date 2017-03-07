d7b80dc5ffa6f26bad39201032f7781105e4e9a6
Adrian <adimandevelopment@gmail.com>
2017-03-07 16:45:48 +0000

    feedback on checkout page if cart is empty

--- js/checkout/checkout.component.js
+++ js/checkout/checkout.component.js
@@ -7,38 +7,40 @@ const CheckoutComponent = {
       cart="$ctrl.cart"
       remove-item="$ctrl.removeItem(item)">
     </cart-list>
-    <div class="total-price">
+    <div class="empty-cart" ng-if="!$ctrl.cart">
+      Cart Is Empty
+    </div>
+    <div class="total-price" ng-if="$ctrl.cart.length">
       Total: {{$ctrl.getTotal() | currency:$:0}}
-    </div>
-    <button ng-click="$ctrl.stripeBuy()">Buy</button>
-    <button ng-click="$ctrl.paypalBuy()">Paypal Buy</button>
+      <button ng-click="$ctrl.stripeBuy()">Buy</button>
+      <button ng-click="$ctrl.paypalBuy()">Paypal Buy</button>
 
-
-    <h4>Payment</h4>
-    <div class="payment-field">
-      <input ng-model="$ctrl.payment.name" type="text" placeholder="Name">
-      <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
-      <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
-    </div>
-    <div class="payment-field">
-      <input ng-model="$ctrl.payment.card.number" type="text" placeholder="Card Number">
-      <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
-      <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
-    </div>
-    <div class="payment-field">
-      <input ng-model="$ctrl.payment.card.cvc" type="text" placeholder="CVC">
-      <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
-      <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
-    </div>
-    <div class="payment-field">
-      <input ng-model="$ctrl.payment.card.exp_month" type="text" placeholder="Exp Month">
-      <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
-      <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
-    </div>
-    <div class="payment-field">
-      <input ng-model="$ctrl.payment.card.exp_year" type="text" placeholder="Exp Year">
-      <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
-      <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
+      <h4>Payment</h4>
+      <div class="payment-field">
+        <input ng-model="$ctrl.payment.name" type="text" placeholder="Name">
+        <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
+        <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
+      </div>
+      <div class="payment-field">
+        <input ng-model="$ctrl.payment.card.number" type="text" placeholder="Card Number">
+        <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
+        <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
+      </div>
+      <div class="payment-field">
+        <input ng-model="$ctrl.payment.card.cvc" type="text" placeholder="CVC">
+        <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
+        <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
+      </div>
+      <div class="payment-field">
+        <input ng-model="$ctrl.payment.card.exp_month" type="text" placeholder="Exp Month">
+        <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
+        <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
+      </div>
+      <div class="payment-field">
+        <input ng-model="$ctrl.payment.card.exp_year" type="text" placeholder="Exp Year">
+        <i class="fa fa-exclamation-circle cc-error" aria-hidden="true"></i>
+        <i class="fa fa-check-circle cc-good" aria-hidden="true"></i>
+      </div>
     </div>
   `
 };
