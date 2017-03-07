9c64893ea1da15e896db4ec0949236f92eb1293f
Adrian <adimandevelopment@gmail.com>
2017-03-07 18:57:31 +0000

    user feedback on completed courses page when no courses have been completed

--- js/account/components/completed/completed.component.js
+++ js/account/components/completed/completed.component.js
@@ -14,6 +14,12 @@ const Completed = {
       <course-card ng-repeat="course in $ctrl.courses"
         course-data="course">
       </course-card>
+      <div class="not-enrolled"
+        ng-if="!$ctrl.courses.length">
+          You haven't finished any courses yet
+          <br>
+          <a href="/courses/">Check Some Out Here</a>
+      </div>
     </div>
     <div ng-if="!$ctrl.courses">
       <htc-sign-in></htc-sign-in>
