965857fa2912235320636859dfc1784c62144183
Adrian <adimandevelopment@gmail.com>
2017-03-07 18:53:04 +0000

    add feedback to user when they are not enrolled in any courses

--- js/account/components/myCourses/my-courses.component.js
+++ js/account/components/myCourses/my-courses.component.js
@@ -12,6 +12,12 @@ const myCourses = {
     </title-block>
     <div class="course-list" ng-if="$ctrl.courses">
       <course-card ng-repeat="course in $ctrl.courses" course-data="course"></course-card>
+      <div class="not-enrolled"
+        ng-if="!$ctrl.courses.length">
+          You are not currently enrolled in any courses
+          <br>
+          <a href="/courses/">Check Some Out Here</a>
+      </div>
     </div>
     <div ng-if="!$ctrl.courses">
       <htc-sign-in></htc-sign-in>
