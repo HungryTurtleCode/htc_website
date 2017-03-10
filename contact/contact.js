66b9f4e3b7f592515d897db606f3068c2f3e75a0
Adrian <adimandevelopment@gmail.com>
2017-03-10 23:52:55 +0000

    add sendgrid to allow a contact form on the site

---
+++ contact/contact.js
@@ -0,0 +1,82 @@
+//contact
+// TODO GET MY OWN API KEY Fri 10 Mar 2017 11:38:00 PM UTC
+var sg = require('sendgrid')('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');
+var helper = require('sendgrid').mail;
+
+module.exports = function(req, res, next){
+  var response = '';
+  var body = '';
+
+  req.on('data', function(data){
+    body += data;
+  });
+  
+  req.on('end', function(){
+    var request = JSON.parse(body);
+
+    console.log(request);
+
+    sendEmail(
+            request.email, 
+            request.subject, 
+            request.name, 
+            request.message
+        )
+        .then(response => {
+            var response = {
+                success: true
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/json'});
+            res.write(JSON.stringify(response));
+            res.end();
+        })
+        .catch(err => {
+            console.log(err);
+            var response = {
+                success: false
+            }
+
+            res.writeHead(200, {'Content-Type': 'text/json'});
+            res.write(JSON.stringify(response));
+            res.end();
+        });
+
+  });
+}
+
+function sendEmail(from, subject, name, message){
+    var mail = new helper.Mail();
+
+    var from_email = new helper.Email(from, name);
+    mail.setFrom(from_email);
+
+    var personalisation = new helper.Personalization();
+    var to_email = new helper.Email('adrian@hungryturtlecode.com');
+    personalisation.addTo(to_email);
+    mail.addPersonalization(personalisation);
+
+    mail.setSubject('HTC Contact - ' + subject);
+
+    var content = new helper.Content('text/html', '<div style="margin-bottom: 50px;">From: ' + name + '</div><div>' + message + '</div>');
+    mail.addContent(content);
+
+    var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+    });
+
+    return new Promise((resolve, reject) => {
+        sg.API(request, function(error, response) {
+            console.log(response.statusCode);
+            console.log(response.body);
+            console.log(response.headers);
+            if(error){
+                reject(error);
+            }else{
+                resolve(response)
+            }
+        });
+    });
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var common = require('./common/index.js');
 var videoAPI = require('./aws/video.js');
 var purchase = require('./purchase/purchase.js');
+var contact = require('./contact/contact.js');
 var express = require('express');
 
 var app = express();
@@ -10,6 +11,7 @@ app.use(common.setHeaders);
 
 /* Routes */
 app.post('/stripeBuy', purchase.stripeCharge);
+app.post('/contact', contact);
 app.post('/paypalBuy', purchase.paypalCharge);
 app.post('/getVideo', videoAPI);
 app.get('/paypal', purchase.paypalExecute);
