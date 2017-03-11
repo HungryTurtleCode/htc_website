474100e94a015c103b1bb1fd939907ecb55db69f
Adrian <adimandevelopment@gmail.com>
2017-03-11 00:36:14 +0000

    show feedback to user on invalid form states

--- js/contact/components/contactForm/contactForm.component.js
+++ js/contact/components/contactForm/contactForm.component.js
@@ -5,13 +5,28 @@ const ContactForm = {
   template: `
     <form class="contact-form" action="">
       <label>Name:</label>
-      <input type="text" placeholder="Name" ng-model="$ctrl.name"/>
-      <label>Email Address:</label>
-      <input type="text" placeholder="Email" ng-model="$ctrl.email"/>
+      <input
+        type="text"
+        placeholder="Name"
+        ng-model="$ctrl.name"
+        ng-class="{'error': $ctrl.emptyName && !$ctrl.name, 'success': $ctrl.emptyName && $ctrl.name}"/>
+
+      <label>Email Address: <span ng-if="$ctrl.invalidEmail && !$ctrl.validateEmail($ctrl.email)">Email Address Is Invalid</span></label>
+      <input
+        type="text"
+        placeholder="Email"
+        ng-model="$ctrl.email"
+        ng-class="{'error': $ctrl.emptyEmail && !$ctrl.validateEmail($ctrl.email), 'success': $ctrl.emptyEmail && $ctrl.validateEmail($ctrl.email)}"/>
       <label>Subject</label>
-      <input type="text" placeholder="Subject" ng-model="$ctrl.subject"/>
+      <input
+        type="text"
+        placeholder="Subject"
+        ng-model="$ctrl.subject"
+        ng-class="{'error': $ctrl.emptySubject && !$ctrl.subject, 'success': $ctrl.emptySubject && $ctrl.subject}"/>
       <label>Message:</label>
-      <textarea ng-model="$ctrl.msg"></textarea>
+      <textarea
+        ng-model="$ctrl.msg"
+        ng-class="{'error': $ctrl.emptyMessage && !$ctrl.msg, 'success': $ctrl.emptyMessage && $ctrl.msg}"></textarea>
       <span class="feedback" ng-class="{'error': $ctrl.error}">
         {{$ctrl.feedbackText}}
       </span>
--- js/contact/components/contactForm/contactForm.controller.js
+++ js/contact/components/contactForm/contactForm.controller.js
@@ -15,18 +15,55 @@ class ContactFormController{
       message: this.msg
     }
 
-    this.$http.post('http://138.197.119.94/contact', data)
-      .then(response => {
-        if(response.data.success){
-          this.feedbackText = 'Message Sent Successfully';
-          this.error = false;
-          this.resetData();
-        }else{
-          this.feedbackText = 'Something Went Wrong. Try Again';
-          this.error = true;
-        }
-      });
+    if(this.validate(data)){
+      this.$http.post('http://138.197.119.94/contact', data)
+        .then(response => {
+          if(response.data.success){
+            this.feedbackText = 'Message Sent Successfully';
+            this.error = false;
+            this.resetData();
+          }else{
+            this.feedbackText = 'Something Went Wrong. Try Again';
+            this.error = true;
+          }
+        });
+    }
+  }
+  validate(data){
+    this.emptyName = false;
+    this.emptyEmail = false;
+    this.emptySubject = false;
+    this.emptyMessage = false;
+    this.invalidEmail = false;
 
+    let errors = false;
+
+    if(!data.name){
+      this.emptyName = true;
+      errors = true;
+    }
+    if(!data.email){
+      this.emptyEmail = true;
+      errors = true;
+    }
+    if(!this.validateEmail(data.email)){
+      this.invalidEmail = true;
+      errors = true;
+    }
+    if(!data.subject){
+      this.emptySubject = true;
+      errors = true;
+    }
+    if(!data.message){
+      this.emptyMessage = true;
+      errors = true;
+    }
+
+    return !errors;
+  }
+  validateEmail(email){
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
   }
   resetData(){
     this.name = this.email = this.subject = this.msg = '';
