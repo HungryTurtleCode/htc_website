4e7688d14e98bd8b965b1d5060979a5b27af236c
Adrian <adimandevelopment@gmail.com>
2017-03-12 22:55:41 +0000

    add a function to get url params. Will be used in future

--- js/archive/components/archiveList/archiveList.controller.js
+++ js/archive/components/archiveList/archiveList.controller.js
@@ -14,6 +14,16 @@ class ArchiveListController {
       return arr.toLowerCase();
     }
   }
+  getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 }
 
 ArchiveListController.$inject = ['paginationService'];
