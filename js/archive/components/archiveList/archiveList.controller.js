a09c14313f97e0f4353619072b40d1b55febaae4
Adrian <adimandevelopment@gmail.com>
2017-03-12 20:31:20 +0000

    further styling to archive cards

--- js/archive/components/archiveList/archiveList.controller.js
+++ js/archive/components/archiveList/archiveList.controller.js
@@ -3,6 +3,13 @@ class ArchiveListController {
     this.data = courseList;
     console.log(this.data);
   }
+  slugify(name){
+    if(name){
+      let arr = name.split(' ');
+      arr = arr.join('-');
+      return arr.toLowerCase();
+    }
+  }
 }
 
 export default ArchiveListController ;
