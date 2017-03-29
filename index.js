713c46ead307b0f1816c4602e553103136e7ca86
Adrian <adimandevelopment@gmail.com>
2017-03-29 20:41:35 +0000

    updated server to use ssl and redirect all http traffic to https

--- index.js
+++ index.js
@@ -5,8 +5,21 @@ var contact = require('./contact/contact.js');
 var activeCampaign = require('./activeCampaign/ac.js');
 var user = require('./user/index.js');
 var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
 
 var app = express();
+
+/* SSL Setup */
+var privateKey =  fs.readFileSync('/etc/letsencrypt/live/api.hungryturtlecode.com/privkey.pem')
+var cert = fs.readFileSync('/etc/letsencrypt/live/api.hungryturtlecode.com/cert.pem');
+
+var options = {
+    key: privateKey,
+    cert: cert
+}
+
 
 /* APP SETUP */
 app.use(common.setHeaders);
@@ -22,6 +35,16 @@ app.get('/paypal', purchase.paypalExecute);
 app.get('/', common.gethandler)
 
 // Listen on port 80
-app.listen(80, function(){
-  console.log('Server running on port 80/');
-});
+// app.listen(80, function(){
+//   console.log('Server running on port 80/');
+// });
+
+// HTTP to HTTPS redirect
+http.createServer((req, res) => {
+    console.log('REDIRECTING ', req.url);
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+}).listen(80, () => console.log('running on port 80'));
+
+// HTTPS server
+https.createServer(options, app).listen(443, () => console.log('Secure SSL server running on port 443'));
