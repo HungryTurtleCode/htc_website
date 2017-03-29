8ff98b5951124e2599436c0fdf17ab4a7835ea1a
Adrian <adimandevelopment@gmail.com>
2017-03-29 23:51:19 +0000

    refactored the routing for the sign in component into a different component so that ui-router isn't always added to the bundle

--- js/common/signInComponent/index.js
+++ js/common/signInComponent/index.js
@@ -1,22 +1,9 @@
 import angular from 'angular';
 import signInComponent from './signin.component';
 
-import uiRouter from 'angular-ui-router';
-
 const signInComponentComponent = angular
-  .module('signInComponent', [
-    uiRouter
-  ])
+  .module('signInComponent', [])
   .component('htcSignIn', signInComponent)
-  .config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider) => {
-    $stateProvider
-      .state('signin', {
-        url: '/signin',
-        template: '<htc-sign-in></htc-sign-in>'
-      });
-
-    $urlRouterProvider.otherwise('/');
-  }])
   .name;
 
 export default signInComponentComponent;
