a1f6126728fe35d4fbeccf1084547b068e5a7488
Adrian <adimandevelopment@gmail.com>
2017-04-03 00:00:04 +0000

    update GA tracking code to correct ID

--- js/common/trackingPixels/trackingPixels.service.js
+++ js/common/trackingPixels/trackingPixels.service.js
@@ -32,7 +32,7 @@ class PixelService{
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-57008136-3', 'auto');
+        ga('create', 'UA-57008136-6', 'auto');
         ga('send', 'pageview');
 
         this.GAReady();
