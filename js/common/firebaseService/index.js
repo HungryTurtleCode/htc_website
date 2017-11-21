6ea59cc32cd6c581f18c42937f3b7fa04314c59a
Adrian Henry <adrian@headspin.io>
2017-11-21 12:22:50 +0000

    combat facebooks appending of random hash to the url after sign in

--- js/common/firebaseService/index.js
+++ js/common/firebaseService/index.js
@@ -2,6 +2,25 @@ import angular from 'angular';
 import firebaseService from './firebase.service';
 import apiService from './api.service';
 
+
+if (window.location.hash && window.location.hash == '#_=_') {
+  if (window.history && history.pushState) {
+    window.history.pushState("", document.title, window.location.pathname);
+  } else {
+    // Prevent scrolling by storing the page's current scroll offset
+    var scroll = {
+      top: document.body.scrollTop,
+      left: document.body.scrollLeft
+    };
+    window.location.hash = '';
+
+    // Restore the scroll offset, should be flicker free
+
+    document.body.scrollTop = scroll.top;
+    document.body.scrollLeft = scroll.left;
+  }
+}
+
 const firebaseServiceComponent = angular
   .module('firebaseService.module', [
   ])
