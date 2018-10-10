32153125a06ff731fc7511f2456e53b5f9e8be2d
Adrian Henry <adrian@hungryturtlecode.com>
2018-10-10 23:09:04 +0100

    feat: Add jwt authentication along side the existing cookie auth

--- js/common/firebaseService/api.service.js
+++ js/common/firebaseService/api.service.js
@@ -4,6 +4,20 @@ class ApiService{
 
     this.apiVersion = 'v0';
     this.url = this.getBaseUrl() + this.apiVersion;
+
+    this.checkForJWT();
+  }
+  checkForJWT() {
+    var value = "; " + document.cookie;
+    var parts = value.split("; htca=");
+    let token = '';
+    if (parts.length == 2) {
+      token = parts.pop().split(";").shift();
+      localStorage.setItem('htca', JSON.stringify(token));
+    } else {
+      token = JSON.parse(localStorage.getItem('htca')) || '';
+    }
+    this.$http.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   }
   getBaseUrl() {
     if(window.location.hostname === 'localhost'){
@@ -16,22 +30,33 @@ class ApiService{
   post(url, data) {
     return this.$http.post(this.url + url, data)
       .then(res => res.data)
+      .then(this._handleDeauth.bind(this))
       .catch(err => console.error(err));
   }
   get(url) {
     return this.$http.get(this.url + url)
       .then(res => res.data)
+      .then(this._handleDeauth.bind(this))
       .catch(err => console.error(err));
   }
   put(url, data) {
     return this.$http.put(this.url + url, data)
       .then(res => res.data)
+      .then(this._handleDeauth.bind(this))
       .catch(err => console.error(err));
   }
   delete(url) {
     return this.$http.delete(this.url + url)
       .then(res => res.data)
+      .then(this._handleDeauth.bind(this))
       .catch(err => console.error(err));
+  }
+  _handleDeauth(res) {
+    if (res.deauth) {
+      localStorage.setItem('htca', JSON.stringify(''));
+      this.$http.defaults.headers.common['Authorization'] = '';
+    }
+    return res;
   }
 }
 
